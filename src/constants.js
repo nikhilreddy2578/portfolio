c21c7b2d961e51f1d48da8aec26cb4d8f36b33b6
nikhilreddy2578 <85238894+nikhilreddy2578@users.noreply.github.com>
2024-11-12 21:01:38 +0530

    added resume link

--- src/constants.js
+++ src/constants.js
@@ -8,8 +8,8 @@ export const dialogueData = {
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/nikhilreddy2578/portfolio/blob/main/NIKHIL%20REDDY_EDUNURU_Resume%20(5).pdf" target="_blank">Check it out?</a>
+  Contact me at nikhilreddy3599@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
   library: `There are a lot of programming books on my shelves.`,
